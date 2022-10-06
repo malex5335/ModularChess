457dd6b04c66f329e452c0393f8003eab5803daf
Maximilian Rich <maximilian.rich@opitz-consulting.com>
2022-10-06 11:42:09 +0200

    prepare castling

--- src/main/java/de/riagade/modular/chess/Castling.java
+++ src/main/java/de/riagade/modular/chess/Castling.java
@@ -1,4 +1,19 @@
 package de.riagade.modular.chess;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
 public class Castling {
+    Map<Player, CastlingOptions> castlingOptions;
+
+    public Castling() {
+        setCastlingOptions(new HashMap<>());
+        getCastlingOptions().put(Player.WHITE, CastlingOptions.BOTH);
+        getCastlingOptions().put(Player.BLACK, CastlingOptions.BOTH);
+    }
 }
